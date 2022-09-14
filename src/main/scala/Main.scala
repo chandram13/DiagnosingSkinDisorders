aee804d40a9b5fb79b233b7235e6110ab190cd2d
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-14 11:44:22 -0700

    Add files via upload

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,5 +2,30 @@ class diagnosingskindisorders{
   def skinPrognosis(symptom):
     if symptom == "red spots":
       println("These red spots are located often on your face, neck, shoulders, chest, and upper bck. This is acne.")
-    if symptom == ""
+    if symptom == "blister on lips":
+      println("This affliction is a cold sore. Often occurs from fever and bacteria infection.")
+    if symptom == "small bump spot":
+      println("This is considred as a blister, these can be located anywhere on the body.")
+    if symptom == "red itchy welts":
+      println("These are called hives whcih are red and small that are seen as skin patches.")
+    if symptom == "crusty skin patch":
+      println("This is diagnosed as actinic keratosis. Seen as pink in color but has a brown base.")
+    if symptom == "red patchy skin":
+      println("This occurs from the consumption of spicy foods, alcholic beverages, or excess sunlight. Often on the nose area.")
+    if symptom == "red skin lump":
+      println("This is a red, irrated lump that comes from fever and has skin crustiness (may accompany oozing.)")
+    if symptom == "pinkish skin patches":
+      println("This is a latex allergy that comes from a skin reaction because of the allergic side affect.")
+    if symptom == "flaky white patches":
+      println("This condition is eczema and can be commonly on the face or skin parts of the body. Use anti-eczema ointment to alleviate this condition.")
+    if symptom == "grey skin patches":
+      println("These silvery, grey patches is called psoriasis. Often itchy and located on the scalp, elbows, knees, and lower back.")
+    if symptom == "extremely hot red skin":
+      println("This condition is called cellulitis, where the skin is extremely hot and painful to touch. The condition spreads rapidly, so seek medical treatment immediately.")
+    if symptom == "small red spots that have patches":
+      println("These are red rashes that spread from the face to the body called measles.")
+    if symptom == "dome-like pink skin scar":
+      println("This condition is called basal cell carcinoma. This wound easily bleeds and may ooze. May not heal without proper treatment.")
+      end if
+    else: println("Either your skin condition is not common, is an advanced skin condition that needs determatologist assistance, or you have a temporary skin condition.")
 }
